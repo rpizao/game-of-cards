ec065037e31c48b5133442aca7e4afb2302ac759
Rafael Pizão <rpizao@gmail.com>
2023-09-25 22:09:01 -0300

    Finalizando o MVP

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+/* 
+    PENDÊNCIAS FUNCIONAIS
+    - O jogo só terminar quando todas as cartas tiverem sido comparadas, ou seja, será uma melhor de 5 jogadas.
+    - O sorteio da carta da máquina ser feito de forma automática (use o método random!)
+    - Exibir o placar
+    - Trocar a mensagem de vitória do console.log para algum ponto local em destaque no HTML.
+
+    PENDÊNCIAS EXTRAS
+    - A carta jogada poderia "executar" um efeito ao ser movida para o centro da mesa.
+*/
+
 const url_novas_cartas = "https://deckofcardsapi.com/api/deck/new/draw/?count=11";
+let resultadoSistema;
+let resultadoUsuario;
 
 window.onload = function(e) {
-    sortearNovasCartas().then(
+    sortearNovasCartas();
+}
+
+function sortearNovasCartas() {
+    limparMesa();
+
+    sortearCartas().then(
         cards => adicionarCartaAoHtml(cards));
 }
 
-async function sortearNovasCartas(){
+function limparMesa(){
+    resultadoSistema = null;
+    resultadoUsuario = null;
+
+    let mesas = document.getElementsByClassName("player");
+    Array.from(mesas).forEach(mesa => {
+        Array.from(mesa.children).forEach(child => mesa.removeChild(child));
+    });
+}
+
+async function sortearCartas(){
     let data = await (await fetch(url_novas_cartas)).json();
     return data.cards;
 }
@@ -14,16 +43,6 @@ function adicionarCartaAoHtml(cards) {
     let mesas = document.getElementsByClassName("player");
 
     Array.from(mesas).forEach(mesa => esconderMesa(mesa));
-    /* let mesaSystem = cards[0];
-    let mesaUser = cards[1];
-
-    for (let i = 0; i < 5; i++) {
-        adicionarCarta(mesaSystem, cards[i]);
-    }
-
-    for (let i = 5; i < 10; i++) {
-        adicionarCarta(mesaUser, cards[i]);
-    } */
 
     if(ehImpar(Array.from(cards).length)){
         console.log("Retiramos a carta " + cards[cards.length - 1].code);
@@ -41,27 +60,71 @@ function adicionarCartaAoHtml(cards) {
 function adicionarCarta(mesa, card){
     let img_card = document.createElement("img");
     img_card.id = card.code;
-    img_card.setAttribute("value", card.value);
-    img_card.addEventListener("click", img => clicouNaCarta(img_card));
-
-    /* <img id="codigo" src="a imagem real ou imagem invertida" content="Caso esteja invertida, é a imagem real."> */ 
-
+    img_card.setAttribute("value", atribuirPesoCarta(card.value)); // QUEEN, JACK, KING, ACE
+    
     if(mesa.classList.contains("system")) {
+        // Cartas do sistema
         img_card.src = "https://deckofcardsapi.com/static/img/back.png";
         img_card.setAttribute("content", card.image);
+        img_card.setAttribute("escondida", "true");
+        img_card.addEventListener("click", img => resultadoSistema = recuperarResultado(img_card));
+        img_card.addEventListener("click", img => clicarCartaMesaSistema(img_card));
     } else {
+        // Cartas do usuário
         img_card.src = card.image;
+        img_card.addEventListener("click", img => resultadoUsuario = recuperarResultado(img_card));
+        img_card.addEventListener("click", img => clicarCartaMesaUsuario(img_card));
     }
     mesa.appendChild(img_card);
 }
 
-function clicouNaCarta(img) {
-    //img.style.marginTop = "-1em";
-    if(img.classList.contains("destaque")) {
-        img.classList.remove("destaque");
-    } else {
-        img.classList.add("destaque");
+function atribuirPesoCarta(valor){
+    if(valor == "ACE"){
+        return 14;
     }
+    if(valor == "KING"){
+        return 13;
+    }
+    if(valor == "JACK") {
+        return 12;
+    }
+    if(valor == "QUEEN") {
+        return 11;
+    }
+    return valor;
+}
+
+function selecionarCarta(img) {
+    // Centralizou a carta selecionada
+    img.classList.add("selecionada");
+}
+
+function clicarCartaMesaUsuario(img) {
+    // Centralizou a carta selecionada
+    selecionarCarta(img);
+
+    // Finaliza o jogo, se ambas as cartas já foram jogadas.
+    finalizarOJogoSePossivel();
+}
+
+function clicarCartaMesaSistema(img) {
+    // Centralizou a carta selecionada
+    selecionarCarta(img);
+    // Exibiu a carta selecionada, se ela estiver escondida
+    revelarCartaEscondida(img);
+
+    // Finaliza o jogo, se ambas as cartas já foram jogadas.
+    finalizarOJogoSePossivel();
+}
+
+function revelarCartaEscondida(img){
+    if(img.hasAttribute("escondida")) {
+        img.src = img.getAttribute("content");
+    }
+}
+
+function recuperarResultado(img) {
+    return Number.parseInt(img.getAttribute("value"));
 }
 
 function exibirMesa(mesa) {
@@ -73,3 +136,25 @@ function esconderMesa(mesa) {
 }
 
 function ehImpar(num) { return num % 2;}
+
+function finalizarOJogoSePossivel() {
+    // TODO: Incluir random para selecionar uma carta na mesa do sistema
+    /* resultadoSistema = recuperarResultado(img_card);
+    clicarCartaMesaSistema(img_card); */
+
+    if(resultadoUsuario == null || resultadoSistema == null){
+        return;
+    }
+
+    if(resultadoUsuario > resultadoSistema) {
+        console.log("Usuário ganhou!!");
+    }
+    else if(resultadoUsuario < resultadoSistema) {
+        console.log("Usuário perdeu!!");
+    } 
+    else {
+        console.log("Empate!");
+    }
+
+    setTimeout(() => sortearNovasCartas(), 8000);
+}
